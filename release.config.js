1bb861488f3d499c7b3868dc587da25f7ec90ea7
Muhammad Zeeshan <iamzeeshan1233@gmail.com>
2024-08-30 06:33:41 +0500

    feat: correct pipes

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+      '@semantic-release/github',
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/git',
+    ],
+  };
+  
